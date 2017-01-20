12d224e135a8588ffdf681cbf2ae6d5a4c998ff4
Afadinsro <odarkwei.mills@ashesi.edu.gh>
2017-01-20 17:20:46 +0000

    Implemented add

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,13 @@
 public class Calculator{
   
   public static void main(String[] args){
-    
+    switch (args[0]){
+      case "add":
+        double a = Double.parseDouble(args[1]);
+        double b = Double.parseDouble(args[2]);
+        System.out.println(a+b);
+        break;
+    }
   }
   
 }
